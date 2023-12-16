049372afb8058fb12bfd53c8f287afedbfeb5ced
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-12-16 23:48:29 +0800

    update check on timestamp dueDate

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# loopback3-assignment
- Task RESTFul API assignment creation using Loopback 3
+# Loopback 3 Assignment
+Backend RESTFul API built using Loopback 3 as an assignment
+
+## Local Setup
+
+### 1. Install dependencies
+
+```
+npm install
+```
+
+### 2. Running project locally
+
+```
+node .
+```
--- common/models/task.js
+++ common/models/task.js
@@ -1,5 +1,27 @@
-'use strict';
+"use strict";
 
-module.exports = function(Task) {
+module.exports = function (Task) {
+  Task.observe("access", async (ctx) => {
+    console.log("Access %s matching %s", ctx.Model.modelName, ctx.query.where);
+  });
 
+  Task.observe("before save", async (ctx) => {
+    const newDueDate = new Date();
+    if (ctx.instance) {
+        console.log(typeof(ctx.instance.title))
+        if(typeof(ctx.instance.title) != "string") {
+            console.log("Title is not a string.")
+            ctx.instance.title = "Default title";
+        }
+        // If due date is set to before today, doesn't make sense
+        // set it to 5 days later
+        if (ctx.instance.dueDate < newDueDate) {
+            newDueDate.setDate(newDueDate.getDate() + 5);
+            ctx.instance.dueDate = newDueDate;
+        }
+    } else {
+        newDueDate.setDate(newDueDate.getDate() + 5);
+        ctx.data.dueDate = newDueDate;
+    }
+  });
 };
